636c25dec817034336a3515e7fcc5631b7f70a04
Domenico-Lo-Giudice <dmnclogiudice@gmail.com>
2023-04-14 16:15:20 +0200

    Add faker + seeder

--- database/seeders/ShoesSeeder.php
+++ database/seeders/ShoesSeeder.php
@@ -5,6 +5,9 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+use App\Models\Shoes;
+use Faker\Generator as Faker;
+
 class ShoesSeeder extends Seeder
 {
     /**
@@ -12,8 +15,17 @@ class ShoesSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for($i = 0; $i < 20; $i++) {
+            $shoe = new Shoes;
+            $shoe -> name = $faker->firstNameMale();
+            $shoe -> brand = $faker->state();
+            $shoe -> size = $faker->numberBetween(0, 55);
+            $shoe -> price = $faker->randomFloat(2, 20, 2000);
+            $shoe -> type = $faker->randomElement(['elegant', 'sportive', 'casual']);
+            $shoe -> img = "https://picsum.photos/300/200";
+            $shoe ->save();
+        } 
     }
 }
